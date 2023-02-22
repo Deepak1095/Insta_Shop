1fe7534312d3c876533a0dcf4d570fec3b56a299
Deepak1095 <ddhankhar999@gmail.com>
2023-02-22 17:25:40 +0530

    footer

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,40 @@
+// import { Input } from '@chakra-ui/react'
+
+// function Navbar(){
+//     return (
+// <Input placeholder='InstaShop.com' htmlSize={50} width='auto'  isInvalid  errorBorderColor='crimson' margin='auto' />
+//     )
+// }
+// export default Navbar
+import React from "react";
+import { Flex, Input, Button } from "@chakra-ui/react";
+import  {BiSearch}  from "react-icons/bi";
+
+const Navbar = () => {
+  return (
+      <Flex padding="0px 20% 20% 20%">
+        <Input
+          bg="#f9fafb"
+          focusBorderColor="black"
+          isInvalid 
+          errorBorderColor="black"
+          placeholder="InstaShop.com"
+          size="md"
+          borderRadius="15px 0px 0px 15px"
+        />
+        <Button
+          color="white"
+          bg="black"
+          size="md"
+          borderRadius="0px 15px 15px 0px"
+          paddingX="10px"
+          _hover="none"
+          _active="none"
+        >
+          <BiSearch fontSize="28px" />
+        </Button>
+      </Flex>
+  );
+};
+
+export default Navbar;
